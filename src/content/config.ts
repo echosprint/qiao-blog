d1bcb011b221fe19b55606aba70c2b8f39bf7b16
QIAO QIAN <qiaoqianda@gmail.com>
2025-05-09 11:01:43 +0800

    footer

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,19 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
-const homeCollection = defineCollection({
-    type: 'content',
+const home = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.md', base: './src/content/home' }),
     schema: z.object({
         title: z.string(),
         role: z.string(),
-        image: z.string()
+        image: z.string(),
+        socialLinks: z.object({
+            x: z.string(),
+            github: z.string()
+        })
     })
 });
 
 export const collections = {
-    'home': homeCollection
+    home: home
 }; 
--- src/content/home/index.md
+++ src/content/home/index.md
@@ -2,6 +2,9 @@
 title: "Hi, I'm Qiao."
 role: "Senior Developer"
 image: "/placeholder.png"
+socialLinks:
+  x: "https://twitter.com/qiaoblog"
+  github: "https://github.com/qiao"
 ---
 
 I'm a professional software developer based in China. I specialize in building modern web applications and creating efficient solutions for complex problems. I'm passionate about technology and enjoy sharing my knowledge through writing and speaking engagements. 
