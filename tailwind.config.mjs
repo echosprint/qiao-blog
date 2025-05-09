7b39797e3e199a0dfbae6b0cb8f4eb5760d44f72
QIAO QIAN <qiaoqianda@gmail.com>
2025-05-09 17:01:16 +0800

    chinese font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Noto Sans SC',
+          'PingFang SC',
+          'Microsoft YaHei',
+          'system-ui',
+          'sans-serif'
+        ],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
