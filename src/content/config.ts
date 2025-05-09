06fb58d29b2bbde8301f5a7f87c4d962d03666b1
QIAO QIAN <qiaoqianda@gmail.com>
2025-05-09 15:43:10 +0800

    baselayout

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,4 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
-
-const home = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: './src/content/home' }),
-    schema: z.object({
-        title: z.string(),
-        role: z.string(),
-        image: z.string(),
-        socialLinks: z.object({
-            x: z.string(),
-            github: z.string()
-        })
-    })
-});
 
 const blogCollection = defineCollection({
   type: 'content',
@@ -27,6 +13,5 @@ const blogCollection = defineCollection({
 });
 
 export const collections = {
-    home: home,
     'blog': blogCollection,
 }; 
