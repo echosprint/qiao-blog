099d4b4300352556fbc63dd03da884201d32e293
QIAO QIAN <qiaoqianda@gmail.com>
2025-05-12 10:55:56 +0800

    base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://echosprint.github.io',
-  base: 'qiao-blog',
+  base: '/qiao-blog',
   vite: {
     plugins: [tailwindcss()]
   }
